9904c989fa27ef5a8d59c641c4c08d5d40fc078b
zreach <1103134401@qq.com>
2025-01-05 15:33:12 +0800

    eval

--- evals/base.py
+++ evals/base.py
@@ -54,7 +54,6 @@ class EvalTool(object):
 
         elif self.calculate_type == 'each_then_overall':
             scores = []
-            dataset_subset = []
             for idx, (data, filtered_dict) in enumerate(tqdm(zip(self.dataset, filtered_responses), total=len(self.dataset))):
                 metric2score = self.dataset.caculate(
                     data,
